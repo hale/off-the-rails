76a2c9f5693b68a2e51f563d48d8fb0fae152e4c
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-02-24 15:07:32 +0000

    Update dating/spec/requests/static_pages_spec.rb

--- dating/spec/requests/static_pages_spec.rb
+++ dating/spec/requests/static_pages_spec.rb
@@ -6,11 +6,7 @@ describe "Static pages" do
   describe "GET /static_pages" do
 
     it "should return http success" do
-<<<<<<< HEAD
-      get static_pages_index_path
-=======
       get '/static_pages/home'
->>>>>>> c5570080867cb4bd81e737e09ecbc5e2a5de7a66
       response.status.should be(200)
     end
 
@@ -19,10 +15,6 @@ describe "Static pages" do
       page.should have_content 'Dating'
     end
 
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> c5570080867cb4bd81e737e09ecbc5e2a5de7a66
   end
 end
