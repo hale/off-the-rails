4c3fc6d22902861044ec34e4467f36a71ba90ebe
Giedrius Lescinskas <g.lescinskas.10@aberdeen.ac.uk>
2012-03-13 00:33:15 +0000

    Status message working - creates a default message, lets update it from home screen. Placed avatar into sidebar

--- dating/app/controllers/users_controller.rb
+++ dating/app/controllers/users_controller.rb
@@ -17,9 +17,30 @@ def create
 end
 
 def home
-	# @title = session[]
+	@user = User.find(session[:user_id])
+	@user_for_status = UserInfo.where(:user_id => session[:user_id]).first
+	if @user_for_status.nil?
+		@user_for_status = UserInfo.new(:user_id => session[:user_id], :status => "You still have no status message")
+		@status = @user_for_status.status
+	else
+		@status = @user_for_status.status
+	end
+	@user_for_status.save
 end
 
+def update_status
+	@user_for_status = UserInfo.where(:user_id => session[:user_id]).first
+	if @user_for_status.nil?
+		@user_for_status = UserInfo.new(:user_id => session[:user_id])
+	end
+	 @user_for_status.status = params[:user_for_status][:status]
+	 if @user_for_status.save
+		flash[:success] = "Status updated"
+		redirect_to :controller => 'users', :id => session[:user_id], :action => 'home' 
+	else
+		render 'update_status'
+	end
+end
 def edit
 	@user = User.find(params[:id])
 	@title = "#{@user.name} - Edit"	
