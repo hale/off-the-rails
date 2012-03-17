005ca67c3ae5aea7f0863e2f670cc885a06a92a3
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-03-17 02:03:42 +0000

    flash notice is now displayed when the user is logged out

--- dating/app/controllers/pages_controller.rb
+++ dating/app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   before_filter :confirm_logged_in, :except => [:home, :help, :about]
   def home
-    session[:user_id] = nil
+    # Visiting the root url logs the user out.
+    unless session[:user_id].nil?
+      session[:user_id] = nil
+      flash.now[:success] = 'You have been logged out.'
+    end
     @title = 'Home'
   end
 
